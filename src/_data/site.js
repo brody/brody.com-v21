bcd3c8b0be8469a320d12ac84c6514866190e159
Brody MacLean <hello@brody.com>
2021-03-16 12:07:12 +1100

    init

---
+++ src/_data/site.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "title": "Brody MacLean",
+  "description": 'Brody MacLean is a Lead Product Designer based in Sydney, Australia.',
+  "url": "https://webstarter.chriscollins.me",
+  "author": "Brody MacLean",
+  "meta_data": {
+    "twitter": "@brodymaclean",
+    "default_social_image": "/static/og.png"
+  },
+  "env": process.env.ELEVENTY_ENV === 'production'
+}
---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  theme: {
+    fontSize: {
+      '12': '0.75rem',
+      '14': '0.875rem',
+      '16': '1rem',
+      '18': '1.125rem',
+      '20': '1.25rem',
+      '22': '1.375rem',
+      '24': '1.5rem',
+      '28': '1.75rem',
+      '32': '2rem'
+    },
+    extend: {
+      colors: {
+        text: 'var(--color-text)',
+        textContrast: 'var(--color-text-contrast)',
+        textSubtle: 'var(--color-text-subtle)',
+        bg: 'var(--color-bg)',
+        bgOffset: 'var(--color-bg-offset)',
+        bgContrast: 'var(--color-bg-contrast)',
+        bgContrast90: 'var(--color-bg-contrast-90)',
+        primary: 'var(--color-primary)',
+        primaryOffset: 'var(--color-primary-offset)',
+        secondary: 'var(--color-secondary)',
+        gradientStart: 'var(--color-gradient-start)',
+        gradientEnd: 'var(--color-gradient-end)'
+      },
+      fontFamily: {
+        var: ['Recursive', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        'grid': '1fr minmax(0, 8ch) min(65ch, 100%) minmax(0, 8ch) 1fr;',
+      },
+      maxWidth: { 
+        '65ch': '65ch', 
+      } 
+    },
+  },
+  variants: {},
+  plugins: [],
+  purge: process.env.NODE_ENV === 'production' ? {
+    enabled: true,
+    content: ['src/**/*.njk', 'src/**/*.js'],
+  } : {} 
+} 
